8e195aff11c72b2d0712f2d13ae25c55e9ad8037
Manvith <yarlagaddamanvithsai@gmail.com>
2025-02-26 09:51:44 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,6 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@aceternity/ui': path.resolve(__dirname, 'node_modules/@aceternity/ui'), // Correct path to node_modules
-    },
-  },
-  server: {
-    watch: {
-      ignored: ['**/tsconfig.json'], // Ignore tsconfig.json to prevent unnecessary reloads
-    },
-  },
+  ]
+ 
 });
